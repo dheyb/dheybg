e2adaa3d9b00cb49951db30bcfb8105b739f120c
dheyb <72129719+dheyb@users.noreply.github.com>
2026-05-20 22:07:32 +0800

    dgxc

--- DCIT26_Lab5/vite.config.js
+++ DCIT26_Lab5/vite.config.js
@@ -1,12 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
-  base: '/my-repo/',
-
-})
-export default defineConfig({
-  plugins: [react(),
-    tailwindcss()
-  ],
-})
+  base: "/DCIT26_Lab5/",
+  plugins: [react(), tailwindcss()],
+});
